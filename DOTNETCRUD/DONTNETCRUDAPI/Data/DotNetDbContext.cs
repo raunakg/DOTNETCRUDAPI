b0487e3158d28b80c769a98c025426ee4dfb2636
raunakg <raunakguptarg@gmail.com>
2022-09-28 12:13:43 +0530

    End of Section 2

---
+++ DOTNETCRUD/DONTNETCRUDAPI/Data/DotNetDbContext.cs
@@ -0,0 +1,20 @@
+﻿using DONTNETCRUDAPI.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace DONTNETCRUDAPI.Data
+{
+    public class DotNetDbContext: DbContext
+    {
+        public DotNetDbContext(DbContextOptions<DotNetDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+
+        public DbSet<WalkDifficulty> walkDifficulties { get; set; }
+
+
+    }
+}
